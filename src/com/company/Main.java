21ffd3818fd8b43e3a9550594504a2e20db75b25
gramsey1 <graceramsey312@yahoo.com>
2018-09-17 11:22:00 -0400

    bye

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
+import java.util.Scanner;
+
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner kbReadingString = new Scanner(System.in);
+        Scanner kbReadingInt = new Scanner(System.in);
+        System.out.println("Yo, type in a word bro");
+        String sign = kbReadingString.next();
+        System.out.println("Yo, type in another word bro");
+        String word1 = kbReadingString.nextLine();
+        String word2 = kbReadingString.nextLine();
+
+
+        if (word1.length() != word2.length()) {
+            System.out.println("no");
+        }
+
     }
 }
+
+
+
+
