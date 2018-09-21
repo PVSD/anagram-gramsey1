12a43bc1691fb8db48edf18bdb392ddef54eed9a
gramsey1 <graceramsey312@yahoo.com>
2018-09-21 10:56:29 -0400

    buddy holly

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,30 @@
 package com.company;
 import java.util.Scanner;
 
-
 public class Main {
 
     public static void main(String[] args) {
         Scanner kbReadingString = new Scanner(System.in);
-        Scanner kbReadingInt = new Scanner(System.in);
         System.out.println("Yo, type in a word bro");
         String sign = kbReadingString.next();
         System.out.println("Yo, type in another word bro");
         String word1 = kbReadingString.nextLine();
         String word2 = kbReadingString.nextLine();
-
-
-        if (word1.length() != word2.length()) {
-            System.out.println("no");
+        if (sign.length() == word2.length()) {
+           char [] array1 = word1.toCharArray();
+            char [] array2 = word2.toCharArray();
+            int word111 = 0;
+            for (int i = 0; i < array1.length; i++) {
+                if (array1[i] == array2[i])
+                    word111++;
+            }
+            if (word111 == array2.length);
+            System.out.println("Your words ARE anagrams of each other! Good job bro.");
         }
-
+        else {
+            System.out.println("Your words AREN'T anagrams of each other. Sorry bro.");
+            System.exit(0);
+        }
     }
 }
 
